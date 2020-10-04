d6925a38386bb8c9c38b15d3e2b4d97aea4e4f4c
Nahum Prashizky <stalkerbear@yahoo.com>
2020-10-04 03:09:04 +0300

    Fix for : Conflict between platform and version check #13 Added tests for fix #13. Works for py2.py3, py2, py3 as well. Fixed test with ujson without version to include --no-source so it won't fall...

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,9 +15,59 @@ def test_download_click_package(tmp_path: Path):
     assert len(files) == 2
 
 
+# Add-on for ignoring unused version - Conflict between platform and version check #13
+# --python-version py3 --python-version cp37 --platform-tag manylinux1_x86_64 --dest . --no-source beautifulsoup4==4.8.2
+# Should download beautifulsoup4-4.8.2-py3-none-any.whl, soupsieve-2.0.1-py3-none-any.whl
+# NOT TO BE DOWNLOADED :  beautifulsoup4-4.8.2-py2-none-any.whl
+def test_download_bs4_package(tmp_path: Path):
+    runner = CliRunner()
+    result = runner.invoke(pipdownload, ["--python-version", "py3", "--python-version", "cp37",
+                                         "--platform-tag", "manylinux1_x86_64", "-d", str(tmp_path), "--no-source",
+                                         "beautifulsoup4==4.8.2"])
+    assert result.exit_code == 0
+    files = list(tmp_path.iterdir())
+    assert len(files) == 2
+
+
+# Add-on for ignoring unused version - Conflict between platform and version check #13
+# --python-version py3 --python-version cp37 --platform-tag manylinux1_x86_64 --dest . --no-source beautifulsoup4==4.8.2
+# Should download beautifulsoup4-4.8.2-py3-none-any.whl, soupsieve-2.0.1-py3-none-any.whl
+# NOT TO BE DOWNLOADED :  beautifulsoup4-4.8.2-py2-none-any.whl
+def test_download_bs4_package_noparam(tmp_path: Path):
+    runner = CliRunner()
+    result = runner.invoke(pipdownload, ["beautifulsoup4==4.8.2", "-d", str(tmp_path)])
+    assert result.exit_code == 0
+    files = list(tmp_path.iterdir())
+    assert len(files) == 5
+
+
+# Add-on for ignoring unused version - Conflict between platform and version check #13
+# --python-version py3 --python-version cp37 --platform-tag manylinux1_x86_64 --dest . --no-source beautifulsoup4==4.8.2
+# Should download beautifulsoup4-4.8.2-py3-none-any.whl, soupsieve-2.0.1-py3-none-any.whl
+# NOT TO BE DOWNLOADED :  beautifulsoup4-4.8.2-py2-none-any.whl
+def test_download_bs4_package_veronly(tmp_path: Path):
+    runner = CliRunner()
+    result = runner.invoke(pipdownload, ["--python-version", "cp37", "beautifulsoup4==4.8.2", "-d", str(tmp_path)])
+    assert result.exit_code == 0
+    files = list(tmp_path.iterdir())
+    assert len(files) == 2
+
+
+# Add-on for ignoring unused version - Conflict between platform and version check #13
+# --python-version py3 --python-version cp37 --platform-tag manylinux1_x86_64 --dest . --no-source beautifulsoup4==4.8.2
+# Should download beautifulsoup4-4.8.2-py3-none-any.whl, soupsieve-2.0.1-py3-none-any.whl
+# NOT TO BE DOWNLOADED :  beautifulsoup4-4.8.2-py2-none-any.whl
+def test_download_pip20_2_3_package(tmp_path: Path):
+    runner = CliRunner()
+    result = runner.invoke(pipdownload, ["--python-version", "py3", "pip==20.2.3", "-d", str(tmp_path)])
+    assert result.exit_code == 0
+    files = list(tmp_path.iterdir())
+    assert len(files) == 2
+
+
 # "redundant" means there are redundant blank lines in requirement file.
 def test_download_from_requirement_file_redundant(
-    requirement_file_redundant, tmp_path: Path
+        requirement_file_redundant, tmp_path: Path
 ):
     runner = CliRunner()
     result = runner.invoke(
@@ -57,10 +107,11 @@ def test_download_with_option_python_versions(tmp_path):
     assert len(files) == 8
 
 
+# Added -no-source because other way round 4 files were returned
 def test_download_with_option_python_versions_and_platform_tags(tmp_path):
     runner = CliRunner()
     result = runner.invoke(
-        pipdownload, ["ujson", "-py", "cp36", "-p", "manylinux", "-d", tmp_path]
+        pipdownload, ["ujson", "-py", "cp36", "-p", "manylinux", "-d", tmp_path, "--no-source"]
     )
     assert result.exit_code == 0
     files = list(tmp_path.iterdir())
@@ -119,7 +170,7 @@ def test_download_with_config_file(tmp_path: Path):
     with open(settings.SETTINGS_FILE, "w") as f:
         json.dump(settings_dict, f, indent=True)
 
-    result = runner.invoke(pipdownload, ["MarkupSafe==1.1.1", "-d", str(tmp_path)])
+    _ = runner.invoke(pipdownload, ["MarkupSafe==1.1.1", "-d", str(tmp_path)])
 
     files = list(tmp_path.iterdir())
     assert len(files) == 2
